abbb3390a1ef62f1ae3e8bc4ae9ccd53caba9bd2
Joey Armstrong <joey@opennetworking.org>
2023-02-13 18:49:47 -0500

    Added scripts to gather basic docker hub image stats

--- docker-hub/stats/pull-stats.sh
+++ docker-hub/stats/pull-stats.sh
@@ -1,21 +1,90 @@
 #!/bin/bash
+## -----------------------------------------------------------------------
+## Intent: Report on image download styats by repository
+## -----------------------------------------------------------------------
 
-# api_v2="https://hub.docker.com/v2/repositories/voltha/voltha-envoy"
+declare -A ARGV=()
+declare -a repos=()
+while [ $# -gt 0 ]; do
+    arg="$1"; shift
+    case "$arg" in
+	-*markup) ARGV['markup']=1 ;;
+	-*repo) repos+=("$1"); shift ;;
+	-*) echo "[SKIP] Unknown switch $arg" ;;
+	*)
+	    if [ -f "$arg" ]; then
+		readarray -t repos < <(cat "$arg")
+	    else
+		repos+=("$arg")
+	    fi
+	    ;;
+    esac
+done
+
+if [ ${#repos[@]} -eq 0 ]; then
+    echo "ERROR: A repository name is required"
+    exit 1
+fi
+
 api_v2="https://hub.docker.com/v2/repositories"
 
-readarray -t names < <(cat docker.names)
 
-declare -A count=()
-for name in "${names[@]}";
+declare -a fields=()
+fields+=('ONF')
+fields+=('hub_user')
+fields+=('status_description')
+fields+=('pull_count')
+fields+=('star_count')
+fields+=('last_updated')
+fields+=('description')
+
+declare -a headers=('Repository' "${fields[@]}")
+
+# HEADER
+echo -n '|| '
+for header in "${headers[@]}";
 do
-    echo "** name: $name"
-    url="${api_v2}/$name"
-    readarray -t ans < <(curl -s "$url" | jq -r ".pull_count")
-    count["$name"]="${ans[0]}"
+    echo -n "$header || "
+done
+echo
+
+declare -A rec=()
+declare -A count=()
+for repo in "${repos[@]}";
+do
+    url="${api_v2}/$repo"
+
+    raw="tmp/raw/${repo}"
+    data="${raw}/data.json"
+    mkdir -p "$raw"
+    curl -o "$data" -s "$url"
+
+    rec=(
+	['Repository']="$repo"
+	['ONF']="$repo"
+    )
+
+    for arg in "${fields[@]}";
+    do
+	readarray -t ans < <(jq -r ".${arg}" "$data")
+	val="${ans[0]}"
+	case "$arg" in
+	    last_updated) val="${val:0:10}" ;;
+	esac
+	rec["$arg"]="$val"
+    done
+
+    echo -n '| '
+    for hdr in "${headers[@]}";
+    do
+	val="${rec[$hdr]}"
+	echo -n " $val | "
+    done
+    echo
+
 done
 
 echo "SUMMARY:"
-# declare -p count
 
 log="voltha.repo.stats"
 cat <<EOF>"$log"
